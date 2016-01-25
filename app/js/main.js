844c88829eef3d81a43a8ad69ff2fc5852829c17
Magomed Kandaev <muhammad-114@mail.ru>
2016-01-25 22:21:07 +0300

    Little corrections after code review with Master

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
-$(function(){
-	console.log('Файл main.js успешно загружен')
-});
+(function($) {
+
+         // DOM Ready
+        $(function() {
+
+            // Binding a click event
+            // From jQuery v.1.7.0 use .on() instead of .bind()
+            $('#add_project').on('click', function(e) {
+
+                // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('#').bPopup();
+
+            });
+
+        });
+
+    })(jQuery);
